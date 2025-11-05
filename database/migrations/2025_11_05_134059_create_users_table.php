12785b835aa03fa113130a25ff2094c128eef82c
Wiranti <wirantioktavianitanwin@gmail.com>
2025-11-05 21:17:58 +0700

    menambahkan migrations dan model

--- database/migrations/2025_11_05_134059_create_users_table.php
+++ database/migrations/2025_11_05_134059_create_users_table.php
@@ -6,25 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->integer('umur')->nullable();
+            $table->float('tinggi_badan')->nullable();
+            $table->enum('jenis_kelamin', ['Laki-laki', 'Perempuan'])->nullable(); 
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
